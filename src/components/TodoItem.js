c26d73c5a27b8bd70daf3f94ee558d91f340ad80
Edward <oluyaratosin123@gmail.com>
2022-01-19 20:16:48 +0100

    Add Todo App functionality from react tutorial

---
+++ src/components/TodoItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TodoItem = (props) =>  {
+    return (
+      <li>
+        <input type="checkbox" 
+        checked={props.todo.completed} 
+        onChange={() => props.handleChangeProps(props.todo.id)}
+        />
+        {props.todo.title}
+        
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+      </li>
+    )
+
+}
+export default TodoItem;
